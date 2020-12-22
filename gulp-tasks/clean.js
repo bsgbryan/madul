7e054e9e15b4f0d481c56cb827f356d95dfbea8a
bsgbryan <bsgbryan@gmail.com>
2020-12-22 02:17:53 -0600

    Removed Gulp-related artifacts

--- gulp-tasks/clean.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-const del = require('del');
-const conf = require('../gulp-conf/base');
-
-gulp.task('clean', function doDel() {
-  return del([conf.paths.tmp, conf.paths.dist]);
-});
--- gulp-tasks/nsp.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const gulp = require('gulp');
-const nsp = require('gulp-nsp');
-const conf = require('../gulp-conf/base');
-
-gulp.task('nsp', function securityCheck(cb) {
-  nsp({package: path.resolve('package.json')}, cb);
-});
