2a2a780780e474556c9fe225e893af66d112a942
bsgbryan <bsgbryan@gmail.com>
2020-12-25 00:24:38 -0600

    Added tests for validate. Udpated tests to use non-arrow functions where appropriate.

--- test/MethodWrapper.test.js
+++ test/MethodWrapper.test.js
@@ -2,10 +2,11 @@ const { expect } = require('chai')
 
 const {
   wrap,
-  doWrap
+  doWrap,
+  validate
 } = require('../lib/MethodWrapper')
 
-const foo = { bar: () => { } }
+const foo = { bar: function() { } }
 
 describe.only('MethodWrapper', () => {
   describe('wrap', () => {
@@ -21,13 +22,13 @@ describe.only('MethodWrapper', () => {
 
     it('wraps all methods on an object', async () => {
       const test = {
-        $init: ({ done, fail }) => {
+        $init: function({ done, fail }) {
           expect(done).to.be.a('function')
           expect(fail).to.be.a('function')
 
           done()
         },
-        foo: ({ testParam, done, fail }) => {
+        foo: function({ testParam, done, fail }) {
           expect(testParam).to.equal('bar')
           expect(done).to.be.a('function')
           expect(fail).to.be.a('function')
@@ -57,8 +58,8 @@ describe.only('MethodWrapper', () => {
 
     it('returns a frozen object', async () => {
       const test = {
-        $init: () => {},
-        foo: () => {}
+        $init: function() { },
+        foo:   function() { }
       }
 
       const wrapped = await wrap(test)
@@ -69,7 +70,7 @@ describe.only('MethodWrapper', () => {
     it('does not wrap methods if they are deps', async () => {
       const test = {
         deps: ['BAR -> bar'],
-        bar: () => 'Not wrapped',
+        bar:  function() { return 'Not wrapped' },
       }
 
       const wrapped = await wrap(test)
@@ -87,6 +88,125 @@ describe.only('MethodWrapper', () => {
       const fn = Object.getPrototypeOf(doWrap()).constructor
 
       expect(fn.name).to.equal('AsyncFunction')
+    })
+
+    it('returns a Promise from the returned AsyncFunction', () => {
+      const fn = Object.getPrototypeOf(doWrap()()).constructor
+
+      expect(fn.name).to.equal('Promise')
+    })
+
+    it('invokes the specified property, using the passed output as "this"', async () => {
+      const output   = { foo: 4 }
+      const instance = { bar: function({ done }) { done(this.foo) } }
+      const wrapped  = doWrap(instance, 'bar', output)
+      const result   = await wrapped()
+
+      expect(result).to.equal(output.foo)
+    })
+
+    it('passes params through to the wrapped function', async () => {
+      const output   = { }
+      const instance = { bar: ({ example, done }) => done(example) }
+      const wrapped  = doWrap(instance, 'bar', output)
+      const result   = await wrapped({ example: 'param' })
+
+      expect(result).to.equal('param')
+    })
+
+    it('rejects the Promise when the wrapped function throws an error', async () => {
+      const output   = { }
+      const instance = { bar: () => { throw new Error('BOOM') } }
+      const wrapped  = doWrap(instance, 'bar', output)
+
+      try {
+        await wrapped()
+      } catch (e) {
+        expect(e.message).to.equal('BOOM')
+      }
+    })
+
+    it('rejects the Promise when the wrapped function calls fail', async () => {
+      const output   = { }
+      const instance = { bar: ({ fail }) => fail('Bahg, bada BOOM') }
+      const wrapped  = doWrap(instance, 'bar', output)
+
+      try {
+        await wrapped()
+      } catch (e) {
+        expect(e.message).to.equal('Bahg, bada BOOM')
+      }
+    })
+
+    it('resolves the Promise when done is called', async () => {
+      const output   = { }
+      const instance = { bar: ({ done }) => done('whew!') }
+      const wrapped  = doWrap(instance, 'bar', output)
+      const result   = await wrapped()
+
+      expect(result).to.equal('whew!')
+    })
+  })
+
+  describe('validate', () => {
+    it('is a function', () =>
+      expect(validate).to.be.a('function')
+    )
+
+    it('throws an error if passed an array', () => {
+      try {
+        validate([])
+      } catch (e) {
+        expect(e.message).to.equal('An array cannot be wrapped')
+      }
+    })
+
+    it('throws an error if passed a string', () => {
+      try {
+        validate('foo')
+      } catch (e) {
+        expect(e.message).to.equal('string is not a valid type')
+      }
+    })
+
+    it('throws an error if passed a number', () => {
+      try {
+        validate(4)
+      } catch (e) {
+        expect(e.message).to.equal('number is not a valid type')
+      }
+    })
+
+    it('throws an error if passed a boolean', () => {
+      try {
+        validate(false)
+      } catch (e) {
+        expect(e.message).to.equal('boolean is not a valid type')
+      }
+    })
+
+    it('throws an error if passed null', () => {
+      try {
+        validate(null)
+      } catch (e) {
+        expect(e.message).to.equal('Cannot wrap null')
+      }
+    })
+
+    it('throws an error if passed undefined', () => {
+      try {
+        validate()
+      } catch (e) {
+        expect(e.message).to.equal('Cannot wrap undefined')
+      }
+    })
+
+    it('throws an error if there are no functions to wrap', () => {
+      try {
+        validate({})
+      } catch (e) {
+        expect(e.message).to.equal('instance must contain at least one functional property')
+      }
     })
   })
 })
