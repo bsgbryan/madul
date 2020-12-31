01ce37e06b6033736b9e047ef716e64d546b6c57
bsgbryan <bsgbryan@gmail.com>
2020-12-30 22:02:33 -0600

    Refactored, added DecoratorManager methods, updated exports

--- sdk/Events.js
+++ sdk/Events.js
@@ -10,13 +10,13 @@ const warn  = (event, args) => emitter.emit(`warn.${event}`, args)
 
 on.call     = (madul, method, callback) => emitter.on(`call.${madul}.${method}`,     callback)
 on.done     = (madul, method, callback) => emitter.on(`done.${madul}.${method}`,     callback)
-on.progress = (madul, method, callback) => emitter.on(`progress.${madul}.${method}`, callback)
 on.error    = (madul, method, callback) => emitter.on(`error.${madul}.${method}`,    callback)
+on.progress = (madul, method, callback) => emitter.on(`progress.${madul}.${method}`, callback)
 
 emit.call     = (madul, method, params) => emitter.emit(`call.${madul}.${method}`,     params)
 emit.done     = (madul, method, params) => emitter.emit(`done.${madul}.${method}`,     params)
-emit.progress = (madul, method, params) => emitter.emit(`progress.${madul}.${method}`, params)
 emit.error    = (madul, method, params) => emitter.emit(`error.${madul}.${method}`,    params)
+emit.progress = (madul, method, params) => emitter.emit(`progress.${madul}.${method}`, params)
 
 exports.on   = on
 exports.emit = emit
