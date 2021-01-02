fafafe5891bff9d7fe937b2a53fce48939164278
Bryan Maynard <bsgbryan@gmail.com>
2021-01-01 22:30:27 -0600

    Added tests, refactored Loader 👍🏻

--- test/DependencyHydrator.test.js
+++ test/DependencyHydrator.test.js
@@ -10,10 +10,29 @@ describe('DependencyHydrator', () => {
       expect(doHydrate).to.be.a('function')
     )
 
-    // it('returns the hydrated dependencies as an object', async () => {
-    //   const output = await doHydrate(['chai'])
+    it('returns the hydrated dependencies as an object', async () => {
+      const output = await doHydrate(['chai'])
 
-    //   expect(Object.keys(output).length).to.equal(1)
-    // })
+      expect(Object.keys(output).length).to.equal(1)
+      expect(Object.keys(output)[0]).to.equal('chai')
+      expect(output.chai).to.be.an('object')
+      expect(output.chai.expect).to.be.a('function')
+    })
+
+    describe('when functions are specified', () => {
+      it('adds each specified function to the output object directly', async () => {
+        const output = await doHydrate(['chai[expect]'])
+
+        expect(Object.keys(output).length).to.equal(1)
+        expect(Object.keys(output)[0]).to.equal('expect')
+        expect(output.expect).to.be.a('function')
+      })
+
+      it('does *not* add the root dependency to the output object', async () => {
+        const output = await doHydrate(['chai[expect]'])
+
+        expect(output.chai).to.be.undefined
+      })
+    })
   })
 })
--- test/Loader.test.js
+++ test/Loader.test.js
@@ -1,7 +1,19 @@
 const { expect } = require('chai')
 
+const { tmpdir } = require('os')
+
 const { SCOPE } = require('../lib/DependencySpec')
 const   load    = require('../lib/Loader')
+const  tmpFile  = require('../lib/Loader/tmpFile')
+
+const wrapped = `
+module.exports = sdk => {
+consoole.log("OHAI")
+
+return exports || module.exports
+
+}
+`
 
 describe('Loader', () => {
   describe('load', () => {
@@ -15,6 +27,28 @@ describe('Loader', () => {
       madul.baz({
         done: sdk => expect(sdk.events.log).to.be.a('function')
       })
+    })
+  })
+
+  describe('tmpFile', () => {
+    it('is a function', () =>
+      expect(tmpFile).to.be.a('function')
+    )
+
+    it('returns the path to the created tmp file', async () => {
+      const path = `${tmpdir()}/madul/scratch/example.js`
+
+      expect(await tmpFile('example')).to.equal(path)
+    })
+  })
+
+  describe('tmpFile.wrap', () => {
+    it('is a function', () =>
+      expect(tmpFile.wrap).to.be.a('function')
+    )
+
+    it('retruns the argument passed wrapped with the code used to pass the sdk to maduls', () => {
+      expect(tmpFile.wrap('consoole.log("OHAI")')).to.equal(wrapped)
     })
   })
 })
