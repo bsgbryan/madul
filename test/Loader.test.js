4d268ad5fb9494988b5d83383a46c4633c5cd76f
bsgbryan <bsgbryan@gmail.com>
2021-01-26 12:42:12 -0600

    Added test, simplified logic

--- test/Loader.test.js
+++ test/Loader.test.js
@@ -2,6 +2,8 @@ const { expect } = require('chai')
 
 const { tmpdir } = require('os')
 
+const { stat } = require('fs').promises
+
 const { assign } = require('../lib/SDKMapping')
 
 const   load    = require('../lib/Loader')
@@ -31,9 +33,30 @@ describe('Loader', () => {
       madul.baz({
         done: sdk => {
           expect(sdk).to.be.an('object')
+          expect(sdk.test).to.be.a('function')
           expect(sdk.test()).to.equal('OHAI')
         }
       })
+    })
+
+    it('properly loads core node modules', async () => {
+      const loaded = await load('os')
+
+      expect(loaded).to.be.an('object')
+      expect(loaded.cpus).to.be.a('function')
+      expect(Array.isArray(loaded.cpus())).to.be.true
+    })
+
+    it('properly loads modules from node_modules', async() => {
+      const loaded = await load('async')
+
+      expect(loaded).to.be.an('object')
+      expect(loaded.each).to.be.a('function')
+    })
+
+    it('throws an error for moduels that cannot be loaded', async () => {
+      try { await load('bad', { root: process.cwd() }) }
+      catch (e) { expect(e.code).to.equal('ENOENT')}
     })
   })
 
@@ -57,5 +80,18 @@ describe('Loader', () => {
     it('retruns the argument passed wrapped with the code used to pass the sdk to maduls', () => {
       expect(tmpFile.wrap('const madul = {}')).to.equal(wrapped)
     })
+  })
+
+  describe('tmpFile.createTmpFileOnlyIfItDoesntExist', async () => {
+    const loadFile = `${tmpdir()}/test.js`
+    const loadPath = tmpdir()
+    const wrapped = 'const test = () => "OHAI"'
+
+    console.log(tmpdir())
+    await tmpFile.createTmpFileOnlyIfItDoesntExist(loadFile, loadPath, wrapped)
+
+    const s = await stat(loadFile)
+
+    expect(s.isFile()).to.be.true
   })
 })
