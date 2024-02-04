e24a7c894da3d46b275dd57ef3125a1e193dd786
Bryan Maynard <bsgbryan@gmail.com>
2024-02-04 12:36:56 -0600

    Added madul.config for testing

---
+++ madul.config.ts
@@ -0,0 +1,12 @@
+import { Input } from "#types"
+
+export const env = () => ({
+  current: process.env.NODE_ENV || 'development',
+  root:    process.cwd(),
+})
+
+export const report = ({ self }: Input) => ({
+  development: `${self.env().root}/.madul/development.report`,
+  production:  `${self.env().root}/.madul/production.report`,
+  test:        `${self.env().root}/.madul/test.report`,
+})
