1cc1b4ef806f769d23c38833516a7ec41fb38854
Bryan Maynard <bsgbryan@gmail.com>
2024-02-07 01:49:11 -0600

    Implemented support for typed output

--- lib/Context.ts
+++ lib/Context.ts
@@ -1,5 +1,7 @@
 import colors from "ansi-colors"
 
+import { WrappedFunction } from "#types"
+
 type Detail = {
   fun: string
   madul: string
@@ -15,11 +17,94 @@ const dim   = colors.dim,
       label = colors.bgRedBright.whiteBright,
       line  = colors.yellowBright,
       madul = colors.bold.cyanBright,
-      name  = colors.whiteBright,
-      str   = colors.whiteBright
+      name  = colors.whiteBright
 
 const seperator = dim('---==========---'),
       key       = (text = '') => `   ${label(`${text.padStart(7)} `)}`
+
+export const func = (type: string, name?: string) => {
+  return name ?
+    [
+      `${colors.cyan('[')}`,
+      `${colors.cyanBright(type)}`,
+      `${colors.dim(':')}`,
+      `${colors.bold.cyanBright(String(name))}`,
+      `${colors.cyan(']')}`,
+    ].join('')
+    :
+    `${colors.cyan(`[${type}]`)}`
+}
+
+export const arr = (items: Array<unknown>): Array<string> => {
+  const width  = String(items.length).length,
+        output = [`${colors.green('Array')}`]
+
+  if (items.length <= 5)
+    return output.concat(
+      items.map((_, i) => [
+        `${colors.gray(String(i).padStart(width))}`,
+        `${colors.dim(':')}`,
+        `${typed(_)}`,
+      ].join(''))
+    )
+  else {
+    const out = items.
+      slice(0, 2).
+      map((_, i) => [
+        `${colors.gray(String(i).padStart(width))}`,
+        `${colors.dim(':')} `,
+        `${typed(_)}`,
+      ].join(''))
+    
+    out.push(`${colors.dim(`${' '.padStart(width)}  ...`)}`)
+
+    return output.concat(
+      out.concat(items.
+        slice(items.length - 2, items.length).
+        map((_, i) => [
+          `${colors.gray(String(i + (items.length - 2)).padStart(width))}`,
+          `${colors.dim(':')} `,
+          `${typed(_)}`,
+        ].join(''))
+      )
+    )
+  }
+}
+
+export const obj = (data: unknown) => {
+  const name = data!.constructor.name === 'Object' ?
+    'object literal'
+    :
+    data!.constructor.name
+
+  const output = [`${colors.green(name)}`],
+        width  = Object.keys(data!).reduce((w, c) => c.length > w ? c.length : w, 0)
+
+  for (const [k, v] of Object.entries(data!)) {
+    output.push(`${colors.white(k.padStart(width))}${colors.dim(':')} ${typed(v)}`)
+  }
+
+  return output
+}
+
+export const typed = (value: unknown) => {
+  switch(typeof value) {
+    case 'string': return `${colors.white(value)}`
+    case 'bigint':
+    case 'number':
+      if (String(value).includes('.'))
+        return `${colors.yellowBright(String(value))}`
+      else return `${colors.yellowBright(String(value))}`
+    case 'object':
+      if (value === null) return `${colors.blue(String(value))}`
+      else if (Array.isArray(value)) return arr(value)
+      else return obj(value)
+    case 'undefined':
+    case 'boolean': return `${colors.blue(String(value))}`
+    case 'function':
+      return func(value.constructor.name, (value as WrappedFunction)._wrapped)
+  }
+}
 
 export const format = (message: string, details: Array<Detail>) => {
   const _ = [`🚨 ${label('  Error ')} ${error(message)}`]
@@ -34,8 +119,8 @@ export const format = (message: string, details: Array<Detail>) => {
     let index = 0
 
     for (const [k, v] of Object.entries(d.params)) {
-      if (index++ === 0) _.push(`${key(n)} ${name(k)}${dim(':')} ${str(v as string)}`)
-      else               _.push(`${key( )} ${name(k)}${dim(':')} ${str(v as string)}`)
+      if (index++ === 0) _.push(`${key(n)} ${name(k)}${dim(':')} ${typed(v as string)}`)
+      else               _.push(`${key( )} ${name(k)}${dim(':')} ${typed(v as string)}`)
     }
 
     _.push(seperator)
---
+++ test/Context.test.ts
@@ -0,0 +1,114 @@
+import {
+  describe,
+  expect,
+  it,
+} from "bun:test"
+
+import { typed } from "#Context"
+
+describe('Context', () => {
+  describe('typed' , () => {
+    it('handles an integer properly', () => {
+      expect(typed(42)).toEqual('\u001B[93m42\u001B[39m')
+    })
+
+    it('handles a float properly', () => {
+      expect(typed(3.14)).toEqual('\u001B[93m3.14\u001B[39m')
+    })
+
+    it('handles a string properly', () => {
+      expect(typed('o, hai')).toEqual('\u001B[37mo, hai\u001B[39m')
+    })
+
+    it('handles a boolean properly', () => {
+      expect(typed(false)).toEqual('\u001B[34mfalse\u001B[39m')
+    })
+
+    it('handles null properly', () => {
+      expect(typed(null)).toEqual('\u001B[34mnull\u001B[39m')
+    })
+
+    it('handles undefined properly', () => {
+      expect(typed(undefined)).toEqual('\u001B[34mundefined\u001B[39m')
+    })
+
+    it('handles an async WrappedFunction properly', () => {
+      const cool = async () => {}
+
+      cool._wrapped = 'cool'
+
+      expect(typed(cool)).toEqual('\u001B[36m[\u001B[39m\u001B[96mAsyncFunction\u001B[39m\u001B[2m:\u001B[22m\u001B[1m\u001B[96mcool\u001B[39m\u001B[22m\u001B[36m]\u001B[39m')
+    })
+
+    it('handles an async function properly', () => {
+      expect(typed(async () => {})).toEqual('\u001B[36m[AsyncFunction]\u001B[39m')
+    })
+
+    it('handles a WrappedFunction properly', () => {
+      const cool = () => {}
+
+      cool._wrapped = 'cool'
+
+      expect(typed(cool)).toEqual('\u001B[36m[\u001B[39m\u001B[96mFunction\u001B[39m\u001B[2m:\u001B[22m\u001B[1m\u001B[96mcool\u001B[39m\u001B[22m\u001B[36m]\u001B[39m')
+    })
+
+    it('handles a function properly', () => {
+      expect(typed(() => {})).toEqual('\u001B[36m[Function]\u001B[39m')
+    })
+
+    it('handles an instance of a class properly', () => {
+      class Cool {}
+      const cool = new Cool()
+
+      expect(typed(cool)).toEqual(['\u001B[32mCool\u001B[39m'])
+    })
+
+    describe('handling of arrays', () => {
+      it('does not truncate when an array has five items', () => {
+        const names = ['alpha', 'beta', 'gamma', 'delta', 'theta']
+
+        expect(typed(names)).toEqual([
+          "\u001B[32mArray\u001B[39m",
+          "\u001B[90m0\u001B[39m\u001B[2m:\u001B[22m\u001B[37malpha\u001B[39m",
+          "\u001B[90m1\u001B[39m\u001B[2m:\u001B[22m\u001B[37mbeta\u001B[39m",
+          "\u001B[90m2\u001B[39m\u001B[2m:\u001B[22m\u001B[37mgamma\u001B[39m",
+          "\u001B[90m3\u001B[39m\u001B[2m:\u001B[22m\u001B[37mdelta\u001B[39m",
+          "\u001B[90m4\u001B[39m\u001B[2m:\u001B[22m\u001B[37mtheta\u001B[39m",
+        ])
+      })
+
+      it('truncates an array with more than five items', () => {
+        const names = [
+          'alpha', 'beta', 'gamma', 'delta', 'theta',
+          'alpha', 'beta', 'gamma', 'delta', 'theta',
+          'alpha', 'beta', 'gamma', 'delta', 'theta',
+          'alpha', 'beta', 'gamma', 'delta', 'theta',
+        ]
+
+        expect(typed(names)).toEqual([
+          "\u001B[32mArray\u001B[39m",
+          "\u001B[90m 0\u001B[39m\u001B[2m:\u001B[22m \u001B[37malpha\u001B[39m",
+          "\u001B[90m 1\u001B[39m\u001B[2m:\u001B[22m \u001B[37mbeta\u001B[39m",
+          "\u001B[2m    ...\u001B[22m",
+          "\u001B[90m18\u001B[39m\u001B[2m:\u001B[22m \u001B[37mdelta\u001B[39m",
+          "\u001B[90m19\u001B[39m\u001B[2m:\u001B[22m \u001B[37mtheta\u001B[39m",
+        ])
+      })
+    })
+
+    it('handles an object literal properly', () => {
+      const cool = {
+        foo: 'bar',
+        baz:  42,
+        boom: false,
+      }
+
+      expect(typed(cool)).toEqual([
+        "\u001B[32mobject literal\u001B[39m",
+        "\u001B[37m foo\u001B[39m\u001B[2m:\u001B[22m \u001B[37mbar\u001B[39m",
+        "\u001B[37m baz\u001B[39m\u001B[2m:\u001B[22m \u001B[93m42\u001B[39m",
+        "\u001B[37mboom\u001B[39m\u001B[2m:\u001B[22m \u001B[34mfalse\u001B[39m",
+      ])
+    })
+  })
+})
